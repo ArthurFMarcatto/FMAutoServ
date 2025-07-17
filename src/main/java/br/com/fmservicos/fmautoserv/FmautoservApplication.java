c56155a86bae97cd5859cb0cbd1ef12ce1870353
Arthur Marcatto <arthurmarcatto@gmail.com>
2025-07-17 20:59:23 -0300

    Criação do Projeto

---
+++ src/main/java/br/com/fmservicos/fmautoserv/FmautoservApplication.java
@@ -0,0 +1,13 @@
+package br.com.fmservicos.fmautoserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmautoservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmautoservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fmservicos/fmautoserv/FmautoservApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fmservicos.fmautoserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmautoservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
